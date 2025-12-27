21b9b6d3ae9b004c7064faa074a1310b895f8577
Klymenko Vladyslav <klima3775@gmail.com>
2025-12-27 18:33:36 +0200

    add mongo connections

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000; 
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env;
+
+const mongoUri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log('✅ Connected to MongoDB');
+  } catch (error) {
+    console.error('❌ MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
 
 app.use(cors());
 app.use(express.json());
+connectToDatabase();
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: "Hello from TypeScript Backend!" });
